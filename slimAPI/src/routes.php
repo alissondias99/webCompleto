bec2d4846d1c08bb4f5eb6b9addfcbcbd8e7616a
Alisson <dias95357@gmail.com>
2025-08-20 10:32:24 -0300

    Finalizando o projeto da API com slim

--- slimAPI/src/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Slim\App;
-use Slim\Http\Request;
-use Slim\Http\Response;
-
-// Routes
-require __DIR__ . '/routes/produtos.php'; // as rotas serão criadas no produtos.php (src→routes→produtos.php)
